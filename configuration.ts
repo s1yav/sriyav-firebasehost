03cdbce74340a6895e16bbfabb57cd0772da1182
s1yav <88557944+s1yav@users.noreply.github.com>
2026-07-18 02:59:50 -0400

    chore: add stackPrefix configuration and ignore .agents directory

--- configuration.ts
+++ configuration.ts
@@ -2,3 +2,4 @@ import * as pulumi from "@pulumi/pulumi";
 
 export const gcpConfig = new pulumi.Config("gcp");
 export const stackName = pulumi.getStack();
+export const stackPrefix = "sriyav-firebase";
